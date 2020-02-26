923b9d389b3fdc599a33a99bb3cb05154cf781c4
Anothery <vilogramme@gmail.com>
2020-02-27 02:44:41 +0300

    Minor build.gradle fix

--- app/src/main/java/com/sudzusama/comparephones/ui/start/StartActivity.kt
+++ app/src/main/java/com/sudzusama/comparephones/ui/start/StartActivity.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sudzusama.comparephones.R
 import com.sudzusama.comparephones.ui.recent.RecentFragment
